87cd1a0ad14820ff26dbb861161e99b91746aa34
Sjors Grijpink <s.grijpink@gmail.com>
2009-08-15 00:00:25 +0200

    Modified import statement to make the project compile

--- tweetview/src/main/scala/com/xebia/model/User.scala
+++ tweetview/src/main/scala/com/xebia/model/User.scala
@@ -1,18 +1,17 @@
 package com.xebia.model
 
-import _root_.Helpers._net.liftweb.mapper._
-import _root_.Helpers._net.liftweb.util._
+import _root_.net.liftweb.mapper._
+import _root_.net.liftweb.util._
 
 /**
  * The singleton that has methods for accessing the database
  */
 object User extends User with MetaMegaProtoUser[User] {
   override def dbTableName = "users" // define the DB table name
-  override def screenWrap = Full(<lift:surround with="default" at="content">
-			       <lift:bind /></lift:surround>)
+  override def screenWrap = Full(<lift:surround with="default" at="content"><lift:bind /></lift:surround>)
+
   // define the order fields will appear in forms and output
-  override def fieldOrder = List(id, firstName, lastName, email,
-  locale, timezone, password, textArea)
+  override def fieldOrder = List(id, firstName, lastName, email, locale, timezone, password, textArea)
 
   // comment this line out to require email validations
   override def skipEmailValidation = true
