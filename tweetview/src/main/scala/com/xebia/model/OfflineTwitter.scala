e15696e1c18adea0665b66779539629be258b939
Arjan Blokzijl <arjan@ulysses.(none)>
2009-08-15 10:23:52 +0200

    added OfflineTwitter for local XML parsing

---
+++ tweetview/src/main/scala/com/xebia/model/OfflineTwitter.scala
@@ -0,0 +1,25 @@
+/*
+ * MockTwitter.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.xebia.model
+
+import _root_.scala.xml.XML
+import _root_.net.liftweb.util._
+
+class OfflineTwitter extends TwitterApi {
+
+    def publicTimeline():Seq[TwitterStatus] = {
+        val xml = XML.load(this.getClass.getResourceAsStream("/twitter_public_timeline.xml"))
+        println("Found xml: " + xml)
+        val statuses = xml \\ "status"
+        statuses.elements.toList.map(s => TwitterStatus.fromXml(s))
+    }
+
+    def timeline(timeline:String):Seq[TwitterStatus] = {
+        Nil
+    }
+}
