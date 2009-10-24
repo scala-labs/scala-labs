8c972dba35041deb4cbc414e972cdc3731892778
Arjan Blokzijl <arjanblokzijl@gmail.com>
2009-10-24 07:10:56 +0200

    Fixed test case for Exercise05

--- labs/src/test/scala/org/scalalabs/basic/lab02/Exercise05Test.scala
+++ labs/src/test/scala/org/scalalabs/basic/lab02/Exercise05Test.scala
@@ -25,7 +25,23 @@ class Exercise05Test extends JUnitSuite {
 
   @Test
   def maxIntInList() {
-    assert(25 === maxElementInList(List(1, 7, 5, 17, 25, 24, 22, 19)))
+    assert(25 === Exercise05.maxElementInList(List(1, 7, 5, 17, 25, 24, 22, 19)))
   }
 
+  @Test
+  def rewriteImperativeToFunctional() {
+     //This unit test succeeds! But, the code that is called is written 'Java style',
+    //it contains a lot of boilerplate code. Your job is to rewrite the code, get rid of the
+    //loops and variabels, and use only functions.
+    val anton1 = Person(15, "Anton1", "Jansen")
+    val anton2 = Person(17, "Anton2", "Janssen")
+    val anton3 = Person(18, "Anton3", "Jansssen")
+    val peter1 = Person(17, "Peter1", "Peterson")
+    val peter2 = Person(19, "Peter2", "Petersson")
+    val jason = Person(21, "Jason", "Jasonsson")
+    
+    val result = Exercise05.separateTheMenFromTheBoys(List(anton1, anton2, anton3, peter1, peter2, jason))
+    assert(List(List("Anton1", "Anton2", "Peter1"), List("Anton3", "Peter2", "Jason")) === result)
+
+  }
 }
