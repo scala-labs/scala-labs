0c23fc3690c5dc5b13197c99a820291d3206a0f4
Lieke van der Poel <liekevdpoel@Liekes-MacBook-Pro.local>
2009-11-27 16:17:48 +0100

    Small changes to the hints, tips and tricks in HelloWorld.scala

--- labs/src/main/scala/org/scalalabs/basic/lab01/HelloWorld.scala
+++ labs/src/main/scala/org/scalalabs/basic/lab01/HelloWorld.scala
@@ -10,18 +10,22 @@ object HelloWorld {
 
   /*
    * This defines the 'sayHello' field of the HelloWorld object.
-   * 
-   * There is no need to create getters and setters to access this value, as is common in Java,
-   * HelloWorld.sayHello will have the same effect.
+   *
+   * Field declarations in Scala are public by default. There is no need to create a getter to access this value, as is
+   * common in Java, HelloWorld.sayHello will have the same effect.
    *
    * Alternative notations:
    * val sayHello = "FixMe"                          -> Type annotation is optional, will be automatically inferred
-   * val sayHello:String = {"FixMe"}                 -> Brackets are also optional
+   * val sayHello: String = {"FixMe"}                 -> Brackets are also optional
+   *
+   * Declaring a value with val will create an immutable field. An immutable field in Scala is the same as a final field
+   * Java. A mutable field can be created by declaring the field with var:
+   * var sayHello: String = "FixMe"
    *
    * More on variable declarations can be found here:
    * http://programming-scala.labs.oreilly.com/ch02.html#VariableDeclarationsAndDefinitions
    */
-	val sayHello:String = "FixMe"
+	val sayHello: String = "FixMe"
 
 
   /*
@@ -31,14 +35,15 @@ object HelloWorld {
    * the method (:String) is again at the end of the method signature, just before the method body.
    *
    * Alternative notations:
-   * def echo(text:String):String = {"FixMe"}        -> Brackets around the method body are optional
-   * def echo(text) = "FixMe"                        -> Type annotation is optional
+   * def echo(text: String): String = {"FixMe"}        -> Brackets around the method body are optional
+   * def echo(text) = "FixMe"                         -> Type annotation is optional
    *
    * Example of a void method:
    * def print(text) { println(text) }               -> note the absence of '=' before the method body
+   * def print(text): Unit = { println(text) }       -> a void method can also have as a return value Unit 
    *
    * More on method declarations can be found here:
    * http://programming-scala.labs.oreilly.com/ch02.html#MethodDeclarationsAndDefinitions
    */
-	def echo(text:String):String = "FixMe"
+	def echo(text: String): String = "FixMe"
 }
