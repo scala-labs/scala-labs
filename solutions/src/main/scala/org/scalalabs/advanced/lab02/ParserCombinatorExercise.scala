2d258ca92f14f0f8744d94ddb295e637362a9ceb
Lieke van der Poel <liekevdpoel@Liekes-MacBook-Pro.local>
2010-04-20 23:09:11 +0200

    First ParserCombinatorExercise

--- solutions/src/main/scala/org/scalalabs/advanced/lab02/ParserCombinatorExercise.scala
+++ solutions/src/main/scala/org/scalalabs/advanced/lab02/ParserCombinatorExercise.scala
@@ -1,5 +1,7 @@
 package org.scalalabs.advanced.lab02
 
+import _root_.scala.util.parsing.combinator.JavaTokenParsers
+
 /**
  * Created by IntelliJ IDEA.
  * User: arjan
@@ -8,4 +10,17 @@ package org.scalalabs.advanced.lab02
  * To change this template use File | Settings | File Templates.
  */
 
-class ParserCombinatorExercise
+class ParserCombinatorExercise extends JavaTokenParsers {
+
+  def particle : Parser[Any] = "the"
+  def noun : Parser[Any] = "fox" | "dog"
+  def adjective: Parser[Any] = "quick" | "brown" | "lazy"
+  def verb: Parser[Any] = "jumps"
+  def preposition: Parser[Any] = "over"
+
+  def nounPhrase : Parser[Any] = particle~rep(adjective)~noun
+  def prepositionPhrase: Parser[Any] = preposition~nounPhrase
+  def verbPhrase: Parser[Any] = verb~prepositionPhrase | verb
+  def sentence: Parser[Any] = nounPhrase~verbPhrase
+
+}
--- solutions/src/test/scala/org/scalalabs/advanced/lab02/ParserCombinatorExerciseTest.scala
+++ solutions/src/test/scala/org/scalalabs/advanced/lab02/ParserCombinatorExerciseTest.scala
@@ -1,5 +1,8 @@
 package org.scalalabs.advanced.lab02
 
+import org.junit.Test
+import org.junit.Assert._
+
 /**
  * Created by IntelliJ IDEA.
  * User: arjan
@@ -8,4 +11,24 @@ package org.scalalabs.advanced.lab02
  * To change this template use File | Settings | File Templates.
  */
 
-class ParserCombinatorExerciseTest
+class ParserCombinatorExerciseTest extends ParserCombinatorExercise {
+
+  @Test
+  def parseSentence {
+    println(parseAll(sentence, "the fox jumps"))
+    assertTrue(parseAll(sentence, "the fox jumps").successful)
+    assertTrue(parseAll(sentence, "the quick fox jumps").successful)
+    assertTrue(parseAll(sentence, "the quick brown fox jumps").successful)
+    assertTrue(parseAll(sentence, "the fox jumps over the dog").successful)
+    assertTrue(parseAll(sentence, "the quick dog jumps over the lazy dog").successful)
+    assertTrue(parseAll(sentence, "the quick brown fox jumps over the lazy dog").successful)
+
+    assertFalse(parseAll(sentence, "the quick brown fox jumps over dog").successful)
+    assertFalse(parseAll(sentence, "fox jumps over the lazy dog").successful)
+    assertFalse(parseAll(sentence, "quick the brown fox jumps over the lazy dog").successful)
+    assertFalse(parseAll(sentence, "jumps the quick brown fox over the lazy dog").successful)
+    assertFalse(parseAll(sentence, "the quick brown fox jumps over the lazy").successful)
+    assertFalse(parseAll(sentence, "the quick brown jumps fox over the lazy dog").successful)
+
+  }
+}
