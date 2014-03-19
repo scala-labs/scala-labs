ee87d0f0338f4c414205ebe49c6ff938fade858e
upeter <peter.urs@gmail.com>
2014-03-19 13:33:30 +0100

    currency converter for implicit paramter exercise

---
+++ labs/src/main/scala/org/scalalabs/basic/lab01/CurrencyConverter.scala
@@ -0,0 +1,15 @@
+package org.scalalabs.basic.lab01
+/**
+ * Classes used in [[org.scalalabs.basic.lab01.OOExerercise]]
+ */
+trait CurrencyConverter {
+  def toEuroCents(dollarCents: Int): Int
+}
+
+trait DefaultCurrencyConverter extends CurrencyConverter {
+  val conversionRate = 1.3598
+  def toEuroCents(dollarCents: Int): Int = 
+		(dollarCents.toDouble * conversionRate).toInt
+}
+
+object DefaultCurrencyConverter extends DefaultCurrencyConverter
