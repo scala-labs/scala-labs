d0b6652b27e5b1cc998e2da3d05963e7a4b41a2e
upeter <peter.urs@gmail.com>
2014-03-20 20:54:12 +0100

    removed obsolote statement

--- solutions/src/main/scala/org/scalalabs/basic/lab04/ImplictConversionExercise02.scala
+++ solutions/src/main/scala/org/scalalabs/basic/lab04/ImplictConversionExercise02.scala
@@ -10,17 +10,16 @@ import scala.util.control._
 object ImplicitConversionExercises02 {
   case class Euro(val euros: Int, val cents: Int)
 
-  object Euro    {
+  object Euro {
     def fromCents(cents: Int) = new Euro(cents / 100, cents % 100)
   }
 
   /**
-    * =======================================================
+   * =======================================================
    */
   object Exercise01 {
 
     class EuroBuilder(val amount: Int, val inCents: Int) {
-      private val stateCents = "cents"
       def euros = new EuroBuilder(0, inCents + amount * 100)
       def cents = new EuroBuilder(0, inCents + amount)
       def apply(amount: Int) = new EuroBuilder(amount, inCents)
