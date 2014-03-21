c10e5281af397a7b6de259147ffc38fd02d0c784
upeter <peter.urs@gmail.com>
2014-03-21 08:15:54 +0100

    Fixed code in uncommented line

--- solutions/src/main/scala/org/scalalabs/basic/lab01/OOExercise.scala
+++ solutions/src/main/scala/org/scalalabs/basic/lab01/OOExercise.scala
@@ -1,5 +1,7 @@
 package org.scalalabs.basic.lab01
+
 import scala.language.implicitConversions
+import org.scalalabs.basic.lab01.DefaultCurrencyConverter
 abstract class Currency(val symbol: String)
 
 class Euro(val euro: Int, val cents: Int = 0) extends Currency("EUR") with Ordered[Euro] {
@@ -22,14 +24,14 @@ object Euro {
     def *(euro: Euro) = euro * i
   }
 
-  //implicit def fromDollar(dollar: Dollar): Euro = Euro.fromCents((DefaultCurrencyConverter.toEuro(dollar.inCents)).toInt)
+  //implicit def fromDollar(dollar: Dollar): Euro = Euro.fromCents((DefaultCurrencyConverter.toEuroCents(dollar.inCents)).toInt)
 
   implicit def fromDollar(dollar: Dollar)(implicit converter:CurrencyConverter): Euro = Euro.fromCents(converter.toEuroCents(dollar.inCents))
   
 
 }
 
-class Dollar(val dollar: Int, val cents: Int = 0) extends Currency("USR") {
+class Dollar(val dollar: Int, val cents: Int = 0) extends Currency("USD") {
   def inCents: Int = dollar * 100 + cents
 }
 
