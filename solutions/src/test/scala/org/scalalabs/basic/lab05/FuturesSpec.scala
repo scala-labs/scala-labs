e3d23063493f725412800c37259198df84e87662
upeter <peter.urs@gmail.com>
2017-09-25 13:18:23 +0200

    Addition of future exercises

---
+++ solutions/src/test/scala/org/scalalabs/basic/lab05/FuturesSpec.scala
@@ -0,0 +1,77 @@
+package org.scalalabs.basic.lab05
+
+import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.concurrent.duration._
+
+class FuturesSpec extends WordSpecLike with Matchers with BeforeAndAfterAll {
+  class CurrencyService(val returnRate: Int)(latency: Int) {
+    def rateUSD: Future[Int] = {
+      Future[Int] {
+        Thread.sleep(latency)
+        returnRate
+      }
+    }
+  }
+  val serviceBankA = new CurrencyService(120)(3000)
+  val serviceBankB = new CurrencyService(123)(2000)
+  val serviceBankC = new CurrencyService(125)(1000)
+  val servicesBankABC = Seq(serviceBankA, serviceBankB, serviceBankC)
+
+  "FuturesExcersise" should {
+    "1: calculate average conversion rate returned by all services" in {
+      val testServices = servicesBankABC
+      val (elapsed, result) = measure {
+        val futures = testServices map { service => service.rateUSD }
+        val res = Future.sequence(futures).map(seq => seq.sum / seq.length)
+        Await.result(res, 5 seconds)
+      }
+      elapsed should be(3000 +- 500)
+      result should be((120 + 123 + 125) / 3)
+    }
+    "2. return first received conversion rate as String" in {
+      val testServices = servicesBankABC
+      val (elapsed, result) = measure {
+        val futures = testServices map { service => service.rateUSD }
+        val future = Future.firstCompletedOf(futures).map(_.toString)
+        Await.result(future, 6 seconds)
+      }
+      elapsed should be(1000 +- 500)
+      result should be(serviceBankC.returnRate.toString)
+    }
+    "3. return first received conversion rate within 2 seconds" in {
+      val bankD = new CurrencyService(120)(4000)
+      val testServices = Seq(serviceBankA, bankD)
+      val (elapsed, result) = measureEither {
+        val futures = testServices map { service => service.rateUSD }
+        val promise = Promise[Int]
+        Future.firstCompletedOf(futures) onSuccess {
+          case value => promise.trySuccess(value)
+        }
+        Future
+        scheduleOnce(2 seconds) {
+          promise.tryFailure(new Exception("timeout"))
+        }
+        Await.result(promise.future, 5 seconds)
+      }
+      elapsed should be(2000 +- 500)
+      result.left.map(_.getMessage) should be(Left("timeout"))
+    }
+    "4. return all conversion rates sequentially using futures" in {
+      val testServices = servicesBankABC
+      def recurse(services: Seq[CurrencyService]): Future[Seq[Int]] = services match {
+        case head :: tail => head.rateUSD.flatMap(res => recurse(tail).map(seq => res +: seq))
+        case Nil => Future(Seq())
+      }
+      val (elapsed, result) = measure {
+        Await.result(recurse(testServices), 8 seconds)
+      }
+      elapsed should be(6000 +- 500)
+      result should be(Seq(120, 123, 125))
+    }
+  }
+
+}
+
---
+++ solutions/src/test/scala/org/scalalabs/basic/lab05/package.scala
@@ -0,0 +1,30 @@
+package org.scalalabs.basic
+
+import java.util.{ Timer, TimerTask }
+
+import scala.concurrent.duration.FiniteDuration
+
+package object lab05 {
+  def measureEither[T](exec: => T): (Int, Either[Throwable, T]) = {
+    import scala.util.control._
+    val start = System.currentTimeMillis()
+    val res = Exception.allCatch.either(exec)
+    val elapsed = System.currentTimeMillis() - start
+    (elapsed.toInt, res)
+  }
+
+  def measure[T](exec: => T): (Int, T) = {
+    val (elapsed, res) = measureEither(exec)
+    elapsed -> res.right.get
+  }
+
+  def scheduleOnce(delay: FiniteDuration)(f: ⇒ Unit) = {
+    val task = new TimerTask {
+      override def run() = f
+    }
+    val timer = new Timer(true);
+    timer.schedule(task, delay.toMillis)
+    timer
+  }
+
+}
