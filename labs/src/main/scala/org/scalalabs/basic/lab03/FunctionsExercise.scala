6f53cad5dcfb16c2c97ae3d3ecbff53fd33f9dae
upeter <peter.urs@gmail.com>
2018-06-28 14:14:40 +0200

    formatting

--- labs/src/main/scala/org/scalalabs/basic/lab03/FunctionsExercise.scala
+++ labs/src/main/scala/org/scalalabs/basic/lab03/FunctionsExercise.scala
@@ -7,22 +7,21 @@ import java.util.Scanner
 import scala.language.reflectiveCalls
 import sys._
 
-
 /**
-  * Higher order functions allow you to build abstractions containing a generic control
-  * structure and a function with which the result(s) of the generic control structure can
-  * be used in different ways.
-  *
-  * Take a look at the predefined methods reverseText() and upperCaseTest().
-  * Both methods contain a lot of duplication which we want to remove.
-  *
-  * Implement the doWithText() method as a higher order function
-  * that takes care of the resource handling of the File and offers a function argument
-  * that allows to deal with the content of the File, which is a String, directly.
-  */
+ * Higher order functions allow you to build abstractions containing a generic control
+ * structure and a function with which the result(s) of the generic control structure can
+ * be used in different ways.
+ *
+ * Take a look at the predefined methods reverseText() and upperCaseTest().
+ * Both methods contain a lot of duplication which we want to remove.
+ *
+ * Implement the doWithText() method as a higher order function
+ * that takes care of the resource handling of the File and offers a function argument
+ * that allows to deal with the content of the File, which is a String, directly.
+ */
 object FunctionsExercise01 {
 
-  def doWithText(/* provide correct function signature */): String = {
+  def doWithText( /* provide correct function signature */ ): String = {
     error("fix me")
   }
 
@@ -47,32 +46,31 @@ object FunctionsExercise01 {
 }
 
 /**
-  * Functions let you separate responsibilities, which allow you to maximally reuse code.
-  *
-  * Create a method measure that accepts any code blocks, executes it and prints the execution time.
-  * E.g. 'The execution took <elapsed> ms'.
-  * Use the logPerf method provided.
-  * Provide a suitable implementation in order to make the corresponding unittest work.
-  */
+ * Functions let you separate responsibilities, which allow you to maximally reuse code.
+ *
+ * Create a method measure that accepts any code blocks, executes it and prints the execution time.
+ * E.g. 'The execution took <elapsed> ms'.
+ * Use the logPerf method provided.
+ * Provide a suitable implementation in order to make the corresponding unittest work.
+ */
 object FunctionsExercise02 {
 
   var printed = ""
 
   private def logPerf(elapsed: Long) = printed = s"The execution took: $elapsed ms"
 
-  def measure[T](/* provide correct method parameter */): T = {
+  def measure[T]( /* provide correct method parameter */ ): T = {
     error("fix me")
   }
 
 }
 
-
 /**
-  * Functions let you create control abstractions, which give extra opportunities to condense
-  * and simplify code.
-  *
-  * Provide a suitable implementation in order to make the corresponding unittest work.
-  */
+ * Functions let you create control abstractions, which give extra opportunities to condense
+ * and simplify code.
+ *
+ * Provide a suitable implementation in order to make the corresponding unittest work.
+ */
 object FunctionsExercise03 {
 
   def plusOne(x: Int): Int = {
--- labs/src/main/scala/org/scalalabs/basic/lab03/PatternMatchingExercise.scala
+++ labs/src/main/scala/org/scalalabs/basic/lab03/PatternMatchingExercise.scala
@@ -3,34 +3,33 @@ package org.scalalabs.basic.lab03
 import sys._
 
 /**
-  * This exercise introduces you to the powerful pattern matching features of Scala.
-  *
-  * Pattern matching can in its essence be compared to Java's 'switch' statement,
-  * even though it provides many more possibilites. Whereas the Java switch statmenet
-  * lets you 'match' primitive types up to int's, Scala's pattern matching goes much
-  * further. Practically everything from all types of objects and Collections
-  * can be matched, not forgetting xml and a special type of class called case classes.
-  *
-  * Pattern matching is also often used in combination with recursive algorithms.
-  *
-  * For this exercise exclusively use pattern matching constructs in order to make the
-  * corresponding unit test work.
-  *
-  * Reference material to solve these exercises can be found here:
-  * Pattern matching in general: http://programming-scala.labs.oreilly.com/ch03.html#PatternMatching
-  * Pattern matching in combination with partial functions: http://programming-scala.labs.oreilly.com/ch08.html#PartialFunctions
-  */
+ * This exercise introduces you to the powerful pattern matching features of Scala.
+ *
+ * Pattern matching can in its essence be compared to Java's 'switch' statement,
+ * even though it provides many more possibilites. Whereas the Java switch statmenet
+ * lets you 'match' primitive types up to int's, Scala's pattern matching goes much
+ * further. Practically everything from all types of objects and Collections
+ * can be matched, not forgetting xml and a special type of class called case classes.
+ *
+ * Pattern matching is also often used in combination with recursive algorithms.
+ *
+ * For this exercise exclusively use pattern matching constructs in order to make the
+ * corresponding unit test work.
+ *
+ * Reference material to solve these exercises can be found here:
+ * Pattern matching in general: http://programming-scala.labs.oreilly.com/ch03.html#PatternMatching
+ * Pattern matching in combination with partial functions: http://programming-scala.labs.oreilly.com/ch08.html#PartialFunctions
+ */
 
 /**
-  * ***********************************************************************
-  * pattern matching exercises
-  * For expected solution see unittest @PatternMatchingExerciseTest
-  * ***********************************************************************
-  */
+ * ***********************************************************************
+ * pattern matching exercises
+ * For expected solution see unittest @PatternMatchingExerciseTest
+ * ***********************************************************************
+ */
 object PatternMatchingExercise01 {
 
   case class Person(name: String, age: Int)
-
 
   def matchOnInputType(in: Any) = {
     error("fix me")
@@ -39,17 +38,16 @@ object PatternMatchingExercise01 {
 }
 
 /**
-  * ***********************************************************************
-  * Partial functions exercise.
-  * The MessageTransformer must use the PartialFunction[Any, Any] called transform to transform messages in its process(msg:Any) method
-  * - For every input message that can be transformed update the count using updateCount(...)
-  * - Messages that cannot be transformed must be returned as is, no count is updated.
-  * Provide an implementation only using PartialFunctions (if statements are not allowed) to make the unittest succeed.
-  * For expected behaviour see @PatternMatchingExerciseTest
-  * ***********************************************************************
-  */
+ * ***********************************************************************
+ * Partial functions exercise.
+ * The MessageTransformer must use the PartialFunction[Any, Any] called transform to transform messages in its process(msg:Any) method
+ * - For every input message that can be transformed update the count using updateCount(...)
+ * - Messages that cannot be transformed must be returned as is, no count is updated.
+ * Provide an implementation only using PartialFunctions (if statements are not allowed) to make the unittest succeed.
+ * For expected behaviour see @PatternMatchingExerciseTest
+ * ***********************************************************************
+ */
 object PatternMatchingExercise02 {
-
 
   class MessageTransformer(private val transform: PartialFunction[Any, Any]) {
 
@@ -64,7 +62,6 @@ object PatternMatchingExercise02 {
     }
 
     def transformationCountBy(clazz: Class[_]): Int = transformationCount(clazz)
-
 
   }
 
