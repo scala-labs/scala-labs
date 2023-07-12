0942317523c03108093a00940618ecaaace972de
Anton Lijcklama à Nijeholt <alijcklamaanijeholt@xebia.com>
2023-07-12 13:23:42 +0200

    Removes unused import

--- solutions/src/main/scala/org/scalalabs/advanced/lab03/TypeExercise.scala
+++ solutions/src/main/scala/org/scalalabs/advanced/lab03/TypeExercise.scala
@@ -1,7 +1,5 @@
 package org.scalalabs.advanced.lab03
 
-import org.joda.time.{LocalDate, DateTime}
-
 /** Created by IntelliJ IDEA. User: arjan Date: Apr 9, 2010 Time: 1:52:50 PM To
   * change this template use File | Settings | File Templates.
   */
